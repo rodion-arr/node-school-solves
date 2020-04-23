d07b4ae723fade4f4772d064516bbc5502ce9bda
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2020-04-24 00:00:50 +0300

    Filtered list

--- filtered-ls.js
+++ filtered-ls.js
@@ -1,1 +1,14 @@
 'use strict'
+const fs = require('fs');
+
+fs.readdir(process.argv[2], (err, files) => {
+    if (err) {
+        throw err;
+    }
+
+    for (const fileName of files) {
+        if (fileName.indexOf(`.${process.argv[3]}`) > -1) {
+            console.log(fileName);
+        }
+    }
+});
