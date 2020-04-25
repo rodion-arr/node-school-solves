2b5ed4b88598dd4f96109f7e471deb61419f86fa
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2020-04-26 00:15:38 +0300

    time-server

---
+++ learnyounode/time-server.js
@@ -0,0 +1,10 @@
+'use strict'
+const net = require('net');
+const strftime = require('strftime');
+
+const port = process.argv[2];
+
+const server = net.createServer(function (socket) {
+    socket.end(strftime('%F %H:%M', (new Date()))+"\n");
+})
+server.listen(port);
