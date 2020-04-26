5ea321f0d4ce92af8664d3532eafac1576911635
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2020-04-27 00:53:54 +0300

    http-uppercase

---
+++ learnyounode/http-uppercaserer.js
@@ -0,0 +1,17 @@
+'use strict'
+const http = require('http');
+const map = require('through2-map')
+
+const port = process.argv[2];
+
+const server = http.createServer(function (req, res) {
+    if (req.method !== 'POST') {
+        return res.end('only POST accepted\n');
+    }
+
+    req.pipe(map(function (chunk) {
+        return chunk.toString().toUpperCase();
+      })).pipe(res);
+});
+
+server.listen(port);
