dce6a685615539f8921aef34d9a256cb41a15210
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2020-05-14 23:51:16 +0300

    html stream

---
+++ stream-adventure/html_stream.js
@@ -0,0 +1,12 @@
+'use strict'
+const trumpet = require('trumpet');
+const through = require('through2');
+const tr = trumpet();
+
+const classStream = tr.select('.loud').createStream();
+classStream.pipe(through(function (data, _, next) {
+  this.push(data.toString().toUpperCase());
+  next();
+})).pipe(classStream);
+
+process.stdin.pipe(tr).pipe(process.stdout);
