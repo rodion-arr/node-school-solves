55f297f12cb599117ce1d348dd6ee7cd6b24d5aa
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2020-05-14 23:54:07 +0300

    duplexer

---
+++ stream-adventure/duplexer.js
@@ -0,0 +1,8 @@
+'use strict'
+const spawn = require('child_process').spawn;
+const duplexer = require('duplexer2');
+
+module.exports = function (cmd, args) {
+  const ps = spawn(cmd, args);
+  return duplexer(ps.stdin, ps.stdout);
+};
