a53e1ba778d86786c639487f1a5cb6eb1add739c
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2020-06-10 14:20:37 +0300

    Bugs | jshint

---
+++ bug-clinic/jshint.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var today = 'today';
+
+console.log('date is', timestamp());
+console.log('today is', today);
+
+function timestamp() {
+    var today = Date();
+    return today;
+}
